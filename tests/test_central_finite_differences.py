95beeb10e2dc31d3c04a2213f07fae5b5bdd402e
Nikos Oikou <nikosoikou@wlnss.com>
2015-08-15 15:47:25 +0200

    (false) test forward approx of first order derivative of a linear function

--- tests/test_central_finite_differences.py
+++ tests/test_central_finite_differences.py
@@ -7,7 +7,11 @@ import finitdiff
 class Suite(unittest.TestCase):
     def test_derivative_of_x_squared(self):
         diff = finitdiff.first_order_central(self, lambda x: x, x=1.0, h=0.1)
-        self.assertAlmostEqual(diff,1)
+        self.assertAlmostEqual(diff, 1)
+
+    def test_derivative_of_x_squared(self):
+        diff = finitdiff.first_order_forward(self, lambda x: x, x=1.0, h=0.1)
+        self.assertAlmostEqual(diff, 1)
 
 if __name__ == '__main__':
     unittest.main()
